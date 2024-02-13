ff696ba1f2cc2a9a80d08aa22ffc9b8d8edd0fd2
Lahu-Shinde9370 <148456862+Lahu-Shinde9370@users.noreply.github.com>
2024-02-13 13:23:33 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -4,16 +4,18 @@ import java.sql.*;
 
 public class DBConnection {
 	public static void main(String args[]) throws Exception {
-		String jdbcDriver = "oracle.jdbc.OracleDriver";
-		String databaseUrl = "jdbc:oracle:thin:@localhost:1521:ORCL";
-
+		
 		// Database credentials
 		String username = "system";
 		String password = "tiger";
+		
+		String jdbcDriver = "oracle.jdbc.OracleDriver";
+		String databaseUrl = "jdbc:oracle:thin:@localhost:1521:ORCL";
 
 		Connection connection = null;
 		try {
 			// Register JDBC driver
+			//now oracle version 11g onwords we can't need of loading this jdbc driver, it will load automatically.
 			Class.forName(jdbcDriver);
 
 			// Open a connection
