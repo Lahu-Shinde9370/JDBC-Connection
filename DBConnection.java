c9e3ec5f730798ab1ef366c49807c0f3bc79df12
Lahu-Shinde9370 <148456862+Lahu-Shinde9370@users.noreply.github.com>
2024-02-13 18:37:47 +0530

    create the obj of Statement (i) and also ResultSet interface for retriving and update the data form D/B S/W

--- DBConnection.java
+++ DBConnection.java
@@ -1,36 +1,57 @@
 package com.java.jdbc;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager; 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DBConnection {
 	public static void main(String args[]) throws Exception {
-		
-		// Database credentials
-		String username = "system";
-		String password = "tiger";
-		
-		String jdbcDriver = "oracle.jdbc.OracleDriver";
-		String databaseUrl = "jdbc:oracle:thin:@localhost:1521:ORCL";
 
-		Connection connection = null;
+		// prerequisists credintial
+		String userName = "system";
+		String pwd = "tiger";
+		String jdbcUrl = "jdbc:oracle:thin:@localhost:1521:ORCL";
+
+		// load the driver class
+		Connection con = null;
 		try {
-			// Register JDBC driver
-			//now oracle version 11g onwords we can't need of loading this jdbc driver, it will load automatically.
-			Class.forName(jdbcDriver);
-
-			// Open a connection
-			connection = DriverManager.getConnection(databaseUrl, username, password);
-		} catch (SQLException | ClassNotFoundException e) {
+			// load the driver class.
+			Class.forName("oracle.jdbc.driver.OracleDriver");
+			// create the connection object
+			con = DriverManager.getConnection(jdbcUrl, userName, pwd);
+			
+			// create the statement objecet
+			Statement st = con.createStatement();
+			
+			System.out.println("Connection obj class = " + con.getClass());
+			System.out.println("Statement obj class = " + st.getClass());
+			
+			int count = st.executeUpdate("update emp set SAL=SAL+100 where JOB='CLERK'");
+			System.out.println("Record update successfully" + count);
+			
+			//create the object of ResultSet interface
+			ResultSet rs = st.executeQuery("SELECT SAL FROM EMP WHERE JOB='CLERK'");
+			System.out.println("Record fetch successfully"+rs); 
+			  
+		} catch (SQLException e) { 
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		} finally {
-			// Close the resources
 			try {
-				if (connection != null) {
-					connection.close();
+				if (con != null) {
+					System.out.println("Connection is eastiblished..");
+					con.close();
+
+				} else {
+					System.out.println("Connection isn't eastiblished..");
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
+
 	}
 }
